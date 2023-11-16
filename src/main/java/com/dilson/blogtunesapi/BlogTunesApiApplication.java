33a9f7a3c68703362805ed82b3899bc167f54e43
dilsoncampelo10 <dilsono316@gmail.com>
2023-11-16 15:04:33 -0300

    :tada: init project

---
+++ src/main/java/com/dilson/blogtunesapi/BlogTunesApiApplication.java
@@ -0,0 +1,13 @@
+package com.dilson.blogtunesapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogTunesApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogTunesApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dilson/blogtunesapi/BlogTunesApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dilson.blogtunesapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogTunesApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
