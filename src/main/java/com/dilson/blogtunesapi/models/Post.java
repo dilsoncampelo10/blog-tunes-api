5eff3bc26ac22c9bf97b49dca836f754ac25c46a
dilsoncampelo10 <dilsono316@gmail.com>
2023-11-16 15:28:59 -0300

    model post

---
+++ src/main/java/com/dilson/blogtunesapi/models/Post.java
@@ -0,0 +1,32 @@
+package com.dilson.blogtunesapi.models;
+
+import java.time.LocalDate;
+import java.util.UUID;
+
+import org.hibernate.annotations.CreationTimestamp;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Entity(name = "posts")
+@Data
+public class Post {
+    @Id
+    @GeneratedValue(generator = "UUID")
+    private UUID id;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(nullable = false, columnDefinition = "TEXT")
+    private String description;
+
+    @Column(nullable = true)
+    private String image;
+
+    @CreationTimestamp
+    private LocalDate date;
+}
