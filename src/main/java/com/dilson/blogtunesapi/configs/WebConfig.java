75725f1683202fc9b8e732d9861f940afb7e5c36
dilsoncampelo10 <dilsono316@gmail.com>
2023-11-16 16:09:58 -0300

    config cors

---
+++ src/main/java/com/dilson/blogtunesapi/configs/WebConfig.java
@@ -0,0 +1,10 @@
+package com.dilson.blogtunesapi.configs;
+
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+public class WebConfig implements WebMvcConfigurer {
+    public void addCorsMappings(CorsRegistry registry){
+        registry.addMapping("/**");
+    }
+}
